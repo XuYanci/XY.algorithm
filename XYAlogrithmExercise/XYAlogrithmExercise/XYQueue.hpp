c308b7079554d2b40c596af981619a44360e6274
yanci <grandy.wind@gmail.com>
2019-10-20 11:31:49 +0800

    1. add CircleQueue

--- XYAlogrithmExercise/XYAlogrithmExercise/XYQueue.hpp
+++ XYAlogrithmExercise/XYAlogrithmExercise/XYQueue.hpp
@@ -12,71 +12,60 @@
 #include <stdio.h>
 class MyCircularQueue {
 public:
+    
+    /// 当前队列长度
+    int currentLength;
+    /// 当前队列指针
+    int currentIndex = 0;
+    /// 队列容量 1000
+    int queue[1000];
+    
     /** Initialize your data structure here. Set the size of the queue to be k. */
     MyCircularQueue(int k) {
-        
+        currentLength =  k;
+        for (int i = 0; i < k - 1;i++) {
+            queue[i] = -1;
+        }
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     bool enQueue(int value) {
+        if (currentIndex + 1 >= currentLength)
+            return false;
+        queue[++currentIndex] = value;
         return true;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     bool deQueue() {
-        return true;
+        if (currentIndex > 0) {
+            queue[--currentIndex] = -1;
+            return true;
+        }
+        return false;
     }
     
     /** Get the front item from the queue. */
     int Front() {
-        return 0;
+        // if (currentIndex <= 0) return - 1;
+        return queue[0];
     }
     
     /** Get the last item from the queue. */
     int Rear() {
-        return 0;
+        if (currentIndex < 0) return - 1;
+        return queue[currentIndex];
     }
     
     /** Checks whether the circular queue is empty or not. */
     bool isEmpty() {
-        return true;
+        return currentIndex >= 0 ? true : false;
     }
     
     /** Checks whether the circular queue is full or not. */
     bool isFull() {
-        return true;
+        return (currentIndex + 1 == currentLength ) ? true : false;
     }
 };
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
-
-//MyCircularQueue circularQueue = new MycircularQueue(3); // 设置长度为 3
-//
-//circularQueue.enQueue(1);  // 返回 true
-//
-//circularQueue.enQueue(2);  // 返回 true
-//
-//circularQueue.enQueue(3);  // 返回 true
-//
-//circularQueue.enQueue(4);  // 返回 false，队列已满
-//
-//circularQueue.Rear();  // 返回 3
-//
-//circularQueue.isFull();  // 返回 true
-//
-//circularQueue.deQueue();  // 返回 true
-//
-//circularQueue.enQueue(4);  // 返回 true
-//
-//circularQueue.Rear();  // 返回 4
-
+ 
 #endif /* XYQueue_hpp */
