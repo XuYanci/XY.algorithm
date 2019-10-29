6fcab6d07e92f0ddfd6b203560b241f63bc89de1
Wind Randy <grandy.wind@gmail.com>
2019-10-29 21:08:30 +0800

    commit some

--- XYAlogrithmExercise/XYAlogrithmExercise/LeetCodeSolution.cpp
+++ XYAlogrithmExercise/XYAlogrithmExercise/LeetCodeSolution.cpp
@@ -995,6 +995,19 @@ int LeetCodeSolution::islands_dfs(vector<vector<char> > &grid, int i, int j, siz
     return 0;
 }
 
+//输入：deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+//输出：6
+//解释：
+//可能的移动序列为 "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202"。
+//注意 "0000" -> "0001" -> "0002" -> "0102" -> "0202" 这样的序列是不能解锁的，
+//因为当拨动到 "0102" 时这个锁就会被锁定。
+
+/// 思路:
+/// 四位数，a , b , c , d , 尽量数字靠近
+/// 可以正向，可以反向
+//  必须完美忽略deadends
+
 int LeetCodeSolution::openLock(vector<string> &deadends, string target) {
+    
     return 0;
 }
