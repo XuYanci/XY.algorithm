957253b0b775fc88fcf6895365e6a51f8935d1eb
yanci <grandy.wind@gmail.com>
2020-01-01 22:22:55 +0800

    commit some

---
+++ XYAlogrithmExercise/XYAlogrithmExercise/AVL/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "AVL.h"
+
+//???
+void menu()
+{
+	printf("\n");
+	printf("*********************?????**********************\n");
+	printf(" 1:????????????     ");
+	printf(" 2:????????        ");
+	printf(" 3:??????????\n");
+	printf(" 4:??????????     ");
+	printf(" 5:??????AVL     ");
+	printf(" 6:????????\n");
+	printf("*************************************************");
+}
+/*40 48 8 8 5 12 30 9 10 -1*/
+void main()
+{
+	int num, temp;
+	DataType data;
+	AVLNode p = NULL;
+	Status taller = FALSE, shorter = FALSE;
+	system("mode con: cols=55 lines=20");
+	menu();
+	while (1)
+	{
+		scanf_s("%d", &num);
+		getchar();
+		switch (num)
+		{
+		case 1:
+			printf("\t\t?????????, ????-1????????\n");
+			while (scanf_s("%d", &data))
+			{
+				if (data == -1)
+					break;
+				else
+				{
+					if (insertAVL(&p, data, &taller))
+						printf("%d ??????;", data);
+					else
+						printf("\n%d ???????;\n", data);
+				}
+			}
+			menu();
+			getchar();
+			break;
+		case 2:
+			printf("\n\t\t    ??????????????: ");
+			scanf_s("%d", &data);
+			if (searchAVL(p, data) == FALSE)
+			{
+				printf("\t\t ??????? %d!\n", data);
+			}
+			else
+			{
+				printf("\t\t ?????? %d!\n", data);
+			}
+			menu();
+			getchar();
+			break;
+		case 3:
+			printf("\n\t\t ????????????????: ");
+			scanf_s("%d", &data);
+			if (deleteAVL(&p, data, &shorter))
+				printf("\n\t\t ??????");
+			else
+				printf("\n\t\t ??????");
+			menu();
+			getchar();
+			break;
+		case 4:
+			printf("?????????? :");
+			Inorder(p);
+			menu();
+			break;
+		case 5:
+			printf("???????????????1.yes	2.no??");
+			scanf_s("%d", &temp);
+			getchar();
+			system("cls");
+			switch (temp)
+			{
+			case 1:
+				destroyAVL(&p);
+				printf("\n\t\t\t??????\n\n");
+				menu();
+				break;
+			}
+			break;
+		case 6:
+			exit(0);
+			break;
+		default:
+			menu();
+			break;
+		}
+	}
+}
