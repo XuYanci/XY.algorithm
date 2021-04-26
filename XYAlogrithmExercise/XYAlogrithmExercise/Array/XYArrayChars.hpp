928fc315832ae9fd1b3db75b506b14c1abb47fe5
XuYanci <grandy.wind@gmail.com>
2021-04-26 08:17:28 +0800

    1. 添加onlyOne

--- XYAlogrithmExercise/XYAlogrithmExercise/Array/XYArrayChars.hpp
+++ XYAlogrithmExercise/XYAlogrithmExercise/Array/XYArrayChars.hpp
@@ -35,5 +35,12 @@ class XYArrayChars {
         }
         return k;
     }
+    
+//    在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字
+//    输入：nums = [3,4,3,3]
+//    输出：4
+    int onlyOne(vector<int> &nums) {
+        return 0;
+    }
 };
 #endif /* XYArrayChars_hpp */
